bfae65a678cb81cc45e035777e1c7bef5c5ea23c
Dika Handika <dika.handika@paper.id>
2023-01-19 15:26:23 +0700

    feat: generate library

---
+++ projects/notification/src/lib/notification.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-notification',
+  template: ` <p>notification works!</p> `,
+  styles: [],
+})
+export class NotificationComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ projects/notification/src/lib/notification.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { NotificationComponent } from './notification.component';
+
+@NgModule({
+  declarations: [NotificationComponent],
+  imports: [],
+  exports: [NotificationComponent],
+})
+export class NotificationModule {}
